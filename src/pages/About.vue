c61ab2089c953990a0a8842a6605170ccebba0a2
Chheany Mok <cmok4290@gmail.com>
2020-07-15 10:38:20 -0400

    Fix typo

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,9 +3,9 @@
     <h1>About me</h1>
     <p>
       I'm just a regular individual with a desire to learn and share what I've
-      learned. This website is meant to be a another resource and in the end, I
-      hope you at least gain something out of this. My secret goal is that you
-      become extremely awesome, even though I already know you are.
+      learned. This website is meant to be a resource and in the end, I hope you
+      at least gain something out of this. My secret goal is that you become
+      extremely awesome, even though I already know you are awesome.
     </p>
   </Layout>
 </template>
