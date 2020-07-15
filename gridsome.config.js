0a78e37e938aba39aaa965dafb06c4590e3a4c16
Chheany Mok <cmok4290@gmail.com>
2020-07-15 08:54:11 -0400

    Add initial project config

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,13 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Tower of Code',
+  siteDescription: 'Tower of Code is a free & open source website to learn how to build websites, apps & anything else with code.',
+  siteUrl: 'https://towerofcode.com',
+  pathPrefix: '',
+  titleTemplate: '%s | Tower of Code',
+  plugins: [],
+  templates: {},
+  metadata: {},
+  icon: './src/favicon.png'
 }
