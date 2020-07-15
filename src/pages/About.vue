bb9de976e15f6e02c69f9b60e1f291554dd50a88
Chheany Mok <cmok4290@gmail.com>
2020-07-15 09:49:12 -0400

    Update about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,19 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>About me</h1>
+    <p>
+      I'm just a regular individual with a desire to learn and share what I've
+      learned. This website is meant to be a another resource and in the end, I
+      hope you at least gain something out of this. My secret goal is that you
+      become extremely awesome, even though I already know you are.
+    </p>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'About us'
-  }
-}
+    title: "About me",
+  },
+};
 </script>
