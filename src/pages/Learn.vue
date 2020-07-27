d096adcf2ba5b6daad3102971d69449eb17be9b8
Chheany Mok <cmok4290@gmail.com>
2020-07-27 10:00:52 -0400

    Add Learn.vue page

---
+++ src/pages/Learn.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Learn</h1>
+    <p>Content Coming Soon!</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Learn",
+  },
+};
+</script>
