662ffe398ab750052255a0ef6f2133dae16e6b54
Chheany Mok <cmok4290@gmail.com>
2020-07-27 11:03:12 -0400

    Add Vuetify

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,12 +5,25 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+// You will need to allowlist Vuetify in webpack in order to build.
+const nodeExternals = require("webpack-node-externals");
+
 module.exports = function (api) {
+  api.chainWebpack((config, { isServer }) => {
+    if (isServer) {
+      config.externals([
+        nodeExternals({
+          allowlist: [/^vuetify/],
+        }),
+      ]);
+    }
+  });
+
   api.loadSource(({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
-  })
+  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  })
-}
+  });
+};
